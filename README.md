519bd06e1107cbb72002e19760a63b1c8d14dfa6
BinduKasturi <bindukasturi27@gmail.com>
2019-06-20 16:29:58 +0530

    all files updated

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# ArcadeGame
+# Classic Arcade Game Clone Project
+
+## Table of Contents
+
+- [Instructions](#instructions)
+- [Contributing](#contributing)
+
+## Instructions
+
+Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+
+Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+
+Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+----------------------
+## I have made the below changes to finish the Project
+
+## Downloading the Project
++ I have downloaded the project skeleton from github using the link provided by udacity and extracted the zip file.
++ I have gone through the project code given and tried to run it. But unable to get anything on the screen and understood that the code need to be changed.
++ I have created an array allEnemies[] and a class for player named as Hero. And also an object player was created for Hero class.
+## Functions for Hero class
++ I have defined three functions update(),render() and handleInput() for Hero class.
++ render() function will place the sprite on the canvas.
++ update() function will updates the score and lifeline.
++ handleInput() function will take care of user inputs. It specifies the conditions to not cross the boundary.
++ Four input indications are already given. So, I defined the conditions and calculations for user input.
++ By clicking on left arrow player will move towards left side by 101 units on x-axis. When the player reaches left most area, he can not move forward.
++ By clicking on right arrow player will move towards right side by 101 units on x-axis. When the player reaches right most area, he can not move forward.
++ By clicking on up arrow player will move towards up side by 101 units on x-axis. When the player reaches top most area, he can not move forward.
++ By clicking on down arrow player will move towards down side by 101 units on x-axis. When the player reaches down most area, he can not move forward.
++ If the player touches water area then he will automatically comes to initial position and score will be incremented by 1.
+## Functions for Enemy class
++ In the same way, I have defined two functions update() and render() for Enemy class.
++ render() function will place the enemy sprite on the canvas and it was already defined.
++ update() function will generate a random speed for all three enemies and updates the speed.
++ In case if player touches enemy then dangerCount will be incremented by 1 and player will come to initial position.
++ life line will be reduced for every increment in dangerCount by 2.
++ score and life line is an addon for this project.
++ when the life line is over a message will be displayed. User can play the game again by clicking on the PlayAgain button. Window will be refreshed if player clicks on PlayAgain button.  
++ User can play the game by opening `index.html` file in browser. So please open `index.html` file in chrome.
---
+++ js/app.js
@@ -0,0 +1,142 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/enemy-bug.png';
+};
+// variable to store how many times player touched the enemies
+var dangerCount = 0;
+// variable to store how many times player touched water
+var score = 0;
+// to display score dynamically
+var scorePannel = document.querySelector("#score");
+var h = document.createElement("h1");
+scorePannel.append(h);
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	if (this.x == 0) {
+		this.speed = 100 + Math.floor(Math.random() * 400);
+	}
+	this.x = this.x + this.speed * dt;
+	if (this.x > 500) {
+		this.x = 0;
+		this.speed = 100 + Math.floor(Math.random() * 400);
+	}
+	//if player touches bug then payer comes to initial position.
+	// comparing the positions of player and bug .
+	if (this.y < player.y + 80 && this.y + 80 > player.y && this.x + 60 > player.x && this.x < player.x + 60) {
+		player.x = player.x;
+		player.y = 404;
+		dangerCount = dangerCount + 1;
+	}
+};
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Hero {
+	constructor(x, y, sprite) {
+		this.x = x;
+		this.y = y;
+		this.sprite = 'images/char-cat-girl.png';
+	}
+}
+Hero.prototype.update = function(dt) {
+	life();
+	h.innerHTML = "Score:" + score;
+}
+Hero.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+// positions for enemies
+var enemyLocation = [65, 65 + 80, 65 + 160];
+enemyLocation.map((p) => {
+	var item = new Enemy(0, p, 150);
+	allEnemies.push(item);
+});
+var player = new Hero(202, 410);
+// conditions for left,right,up and down moves for player
+// conditions to not cross boundaries
+Hero.prototype.handleInput = function(key) {
+	if (key == 'left') {
+		this.x = this.x - 101;
+		if (this.x < 0) {
+			this.x = 0;
+		}
+	} else if (key == 'right') {
+		this.x = this.x + 101;
+		if (this.x > 404) {
+			this.x = 404;
+		}
+	} else if (key == 'up') {
+		this.y = this.y - 83;
+		// if player touches water then he will come to initial position automatically
+		if (this.y < 0) {
+			this.y = 0;
+			setTimeout(() => {
+				this.x = this.x;
+				this.y = 410;
+			}, 100);
+			score = score + 1;
+		}
+	} else {
+		this.y = this.y + 83;
+		if (this.y > 410) {
+			this.y = 410;
+		}
+	}
+}
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+	player.handleInput(allowedKeys[e.keyCode]);
+});
+// to display life line
+var lifered = [...document.getElementsByClassName("life")];
+
+function life() {
+	// lf is a variable that specifies the life line count
+	var lf = 5;
+	for (var i = 1; i <= 10; i++) {
+		if (dangerCount >= 2 * i) {
+			lf = lf - 1;
+			lifered[lf].style.display = "none";
+		}
+		// condition to display message after completion of the game
+		if (lf == 0) {
+			var alert = document.getElementById("alert");
+			var span = document.getElementsByClassName("close")[0];
+			alert.style.display = "block";
+			span.onclick = function() {
+				alert.style.display = "none";
+			}
+			window.onclick = function(event) {
+				if (event.target == alert) {
+					alert.style.display = "none";
+				}
+			}
+		}
+	}
+}
